6836e2a72a9f5015b2f5ab8328081c757f8a12cc
Gabriel Mania <maniagp@proton.me>
2024-11-29 11:58:09 +0800

    Initial blog list, detail, and image models

--- oursite/blog/models.py
+++ oursite/blog/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.db import models
+from modelcluster.models import ParentalKey
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogIndexPage(Page):
+    description = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel(
+            "description",
+        )
+    ]
+
+
+class BlogPostPage(Page):
+    date = models.DateField("Post Date", default=date.today)
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+
+
+class BlogPageImageGallery(Orderable):
+    page = ParentalKey(BlogPostPage, related_name="image_gallery", on_delete=models.CASCADE)  # type: ignore
+    image = models.ForeignKey(
+        "wagtailimages.Image", related_name="+", on_delete=models.CASCADE
+    )
+    caption = models.CharField(max_length=255, blank=True)
